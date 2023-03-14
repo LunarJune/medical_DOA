cff66bcd505acd86b29b063dc515a82c5959d459
Admin <Admin@PF2VXR7P-ENS.inc.bytedance.com>
2023-03-14 20:19:15 +0800

    commmit

--- src/main/java/xly/Main.java
+++ src/main/java/xly/Main.java
@@ -1,17 +1,27 @@
 package xly;
 
-import java.io.OutputStream;
+import java.io.*;
 import java.net.InetAddress;
 import java.net.Socket;
 import com.google.gson.*;
+import xly.doip.*;
 
 public class Main {
-    private static class Test{
-        public String tt = "hhhh";
-    }
     public static void main(String[] args) throws Exception {
+        String str = "{\"a\":\"b\nb\nb\"}";
         Gson gson = new Gson();
-        Test test = new Test();
-        System.out.println(gson.toJson(test));
+
+//        OutputStream out = new FileOutputStream("tmp.txt");
+//        OutDoipMessage outd = new OutDoipMessageImpl(out);
+//        JsonObject json = new JsonObject();
+//        json.addProperty("a","b");
+//        outd.writeJson(json);
+//        outd.writeBytes("#\n#\n#".getBytes());
+//        outd.close();
+//        InputStream in = new FileInputStream("tmp.txt");
+//        InDoipMessage ind = new InDoipMessageImpl(in);
+//        for(InDoipSegment seg: ind){
+//            Util.printStream(seg.getInputStream());
+//        }
     }
 }
--- src/main/java/xly/test/SocketTCP_Client.java
+++ src/main/java/xly/test/SocketTCP_Client.java
@@ -3,25 +3,27 @@ package xly.test;
 import java.io.OutputStream;
 import java.net.InetAddress;
 import java.net.Socket;
+import java.util.*;
 
 /**
  * 客户端
  */
 public class SocketTCP_Client {
     public static void main(String[] args) throws Exception {
-        //1. 连接服务端 (ip , 端口）
-        //解读:连接本机的 9999端口, 如果连接成功，返回Socket对象
-        Socket socket = new Socket(InetAddress.getByName("192.168.0.109"), 9999);
+        String ip = "127.0.0.1";
+        int port = 8888;
+        Socket socket = new Socket(InetAddress.getByName(ip), port);
         System.out.println("客户端 socket = " + socket.getClass());
-        //2. 连接上后，生成Socket, 通过socket.getOutputStream()
-        //得到和 socket对象关联的输出流对象
+        String message;
+        Scanner scanner = new Scanner(System.in);
         OutputStream outputStream = socket.getOutputStream();
-        //3. 通过输出流，写入数据到 数据通道
-        outputStream.write("Hello,server".getBytes());
-        //4. 关闭流对象和socket, 必须关闭
-        outputStream.close();
-        socket.close();
-        System.out.println("客户端退出...");
+        while (true) {
+            message = scanner.nextLine();
+            outputStream.write(message.getBytes());
+        }
+//        outputStream.close();
+//        socket.close();
+//        System.out.println("客户端退出...");
     }
 
 }
