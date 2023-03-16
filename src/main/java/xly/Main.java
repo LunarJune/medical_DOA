3586dcca9c4e5d766a685b6fb0354270aaf651f8
xiaoly <245594435@qq.com>
2023-03-16 23:31:30 +0800

    commit

--- src/main/java/xly/Main.java
+++ src/main/java/xly/Main.java
@@ -3,10 +3,13 @@ package xly;
 import java.io.*;
 import java.net.InetAddress;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Scanner;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import com.google.gson.*;
 import xly.doip.*;
+import xly.doip.client.*;
 import xly.doip.client.transport.*;
 import xly.doip.util.*;
 
@@ -18,23 +21,10 @@ public class Main {
         connectionOptions.port = 8888;
         DoipConnectionPool pool = new DoipConnectionPool(10,client,connectionOptions);
         DoipConnection conn1 = pool.get();
-        DoipConnection conn2 = pool.get();
+//        conn1.
         pool.release(conn1);
-        DoipConnection conn3 = pool.get();
+        DoipConnection conn2 = pool.get();
         System.out.println(conn1.getSocket().getLocalPort());
         System.out.println(conn2.getSocket().getLocalPort());
-        System.out.println(conn3.getSocket().getLocalPort());
-//        OutputStream out = new FileOutputStream("tmp.txt");
-//        OutDoipMessage outd = new OutDoipMessageImpl(out);
-//        JsonObject json = new JsonObject();
-//        json.addProperty("a","b");
-//        outd.writeJson(json);
-//        outd.writeBytes("#\n#\n#".getBytes());
-//        outd.close();
-//        InputStream in = new FileInputStream("tmp.txt");
-//        InDoipMessage ind = new InDoipMessageImpl(in);
-//        for(InDoipSegment seg: ind){
-//            Util.printStream(seg.getInputStream());
-//        }
     }
 }
